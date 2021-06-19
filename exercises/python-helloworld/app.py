9422034f3e1b9ee3ff04a686322e0a8258d9a25f
Ítalo Sjobom <italosjobom@MacBook-Pro-de-Italo.local>
2021-06-19 19:24:25 -0300

    Exercise  and the solution

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def statusResponse():
+    return '{} ; {} ; {}'.format("Status Code: 200", "JSON: {'user': 'admin'}", "result: OK - healthy")
+
+@app.route("/metrics")
+def metricsResponse():
+    return '{} ; {}'.format("JSON: {'user': 'admin'}", "data: {UserCount: 140, UserCountActive: 23}")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+    app.run(port='5000')
