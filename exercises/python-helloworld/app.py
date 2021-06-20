fff2b4bac2ebb429b54bbb9f26e570cbd2a0e1fc
Ítalo Sjobom <italosjobom@MacBook-Pro-de-Italo.local>
2021-06-20 16:21:39 -0300

    Exercise logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,18 +1,44 @@
 from flask import Flask
+from flask import json
+import logging
+import time
+
 app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    
+    timestamp = time.time()
+    endPointName = "status"
+    logging.debug("%s, %s endpoint was reached", timestamp, endPointName)
+
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json',
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    
+    timestamp = time.time()
+    endPointName = "metrics"
+    logging.debug("%s, %s endpoint was reached", timestamp, endPointName)
+
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
 
 @app.route("/")
 def hello():
+    logging.basicConfig(level=logging.DEBUG, filename="app.log", filemode="w", format='%(name)s - %(levelname)s - %(message)s')
     return "Hello World!"
-
-@app.route("/status")
-def statusResponse():
-    return '{} ; {} ; {}'.format("Status Code: 200", "JSON: {'user': 'admin'}", "result: OK - healthy")
-
-@app.route("/metrics")
-def metricsResponse():
-    return '{} ; {}'.format("JSON: {'user': 'admin'}", "data: {UserCount: 140, UserCountActive: 23}")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    app.run(port='5000')
